3a2364c9cd361afc00d915262642663da8a1de1c
Hassan <106278943+hassan-learning21@users.noreply.github.com>
2023-06-17 00:08:03 +0500

    -

--- template/header.php
+++ template/header.php
@@ -7,6 +7,7 @@
     <title><?php echo $title; ?></title>
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://use.fontawesome.com/3f38d47325.js"></script>
     <!-- JS Files -->
     <script src="assets/js/main.js"></script>
     <!-- CSS Files -->
@@ -16,7 +17,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <!-- Favicon -->
     <link rel="icon" type="image" href="https://i.postimg.cc/XN52V4pJ/fav.png">
 </head>
@@ -24,18 +25,30 @@
 <body>
     <header>
         <div class="header-primary">
-            <div class="fl-row row-fixed">
+            <div class="fl-row row-fixed top-bar">
+                <div class="col-group col-of-3">
+                    <div class="col"></div>
+                    <div class="col"><a href="+tel:+923379222295"><i class="fa fa-envelope" aria-hidden="true"></i> (+92)3379222295</a></div>
+                    <div class="col"><a href="mailto:hassanahmed12@live.com"><i class="fa fa-phone" aria-hidden="true"></i> hassanahmed12@live.com</a></div>
+                </div>
+            </div>
+            <div class="fl-row row-fixed hdr-row" >
                 <div class="col-group col-of-3">
                     <div class="col"><div class="main-logo"><a href="/spectron"><img src="https://i.postimg.cc/wTdwMZmk/logo.png" alt="logo" width="200px"></a></div></div>
                     <div class="col">
                         <nav class="head-menu">
                             <ul>
-                                <li><a href="index.php">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Portfolio</a></li>
                                 <li><a href="contact.php">Contact</a></li>
                             </ul>
                         </nav>
                     </div>
-                    <div class="col"><a href="#">Get Started</a></div>
+                    <div class="col">
+                        <div class="btn-box">
+                            <a href="#">Get Started</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
