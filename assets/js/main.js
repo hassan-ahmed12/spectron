3b5135f28dba197df28d0246d33030e0dbbc8f5b
Hassan <106278943+hassan-learning21@users.noreply.github.com>
2023-08-03 00:33:08 +0500

    -

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,45 @@
-// alert("main.js loaded");
-
-//Loading header through jquery, this loads the whole header.html or element with #header id to the div with #dynamic-header id in any other html file.
-// $(function(){
-//     $("#dynamic-header").load("template/header.html #header");
-//   });
-
+//
 function btnfun() {
-    alert("Fuck You!");
+    alert("Fuck Off!");
   }
 
-  function MobileMenu() {
-    var x = document.getElementById("hdr-menu");
-    if (x.className === "head-menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "head-menu";
-    }
-
-    var x = document.getElementById("m-nav-btn");
-    if (x.className === "m-nav-btn") {
-      x.className += " draw";
-    } else {
-      x.className = "m-nav-btn";
-    }
-
-    var x = document.getElementById("hamburger");
-    if (x.className === "hamburger") {
-      x.className += " hamburger-hide";
-    } else {
-      x.className = "hamburger";
-    }
-
-    var x = document.getElementById("menu-close");
-    if (x.className === "menu-close") {
-      x.className += " menu-close-show";
-    } else {
-      x.className = "menu-close";
-    }
-
-    var x = document.getElementById("menu-logo");
-    if (x.className === "menu-logo") {
-      x.className += " top";
-    } else {
-      x.className = "menu-logo";
-    }
-
+  //Mobile Menu 
+function MobileMenu() {
+  var x = document.getElementById("hdr-menu");
+  if (x.className === "head-menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "head-menu";
   }
+
+  var x = document.getElementById("m-nav-btn");
+  if (x.className === "m-nav-btn") {
+    x.className += " draw";
+  } else {
+    x.className = "m-nav-btn";
+  }
+
+  var x = document.getElementById("hamburger");
+  if (x.className === "hamburger") {
+    x.className += " hamburger-hide";
+  } else {
+    x.className = "hamburger";
+  }
+
+  var x = document.getElementById("menu-close");
+  if (x.className === "menu-close") {
+    x.className += " menu-close-show";
+  } else {
+    x.className = "menu-close";
+  }
+
+  var x = document.getElementById("menu-logo");
+  if (x.className === "menu-logo") {
+    x.className += " top";
+  } else {
+    x.className = "menu-logo";
+  }
+}
 
 // Jquery code to add active class on menu by url
 $(document).ready(function() {
